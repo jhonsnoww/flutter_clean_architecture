405c2af860da53c14f8650b2b44151a73075c788
jhonsnoww <oochanbg@gmail.com>
2021-03-29 17:04:13 +0630

    add usecase

--- test/features/number_triviar/domain/usecases/get_concrete_number_test.dart
+++ test/features/number_triviar/domain/usecases/get_concrete_number_test.dart
@@ -1,5 +1,14 @@
 import 'package:demo_clean_architecture/features/number_trivia/domain/respositories/number_triviar_respository.dart';
+import 'package:demo_clean_architecture/features/number_trivia/domain/usecases/get_concrete_number.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 class MockNumberTriviarRespository extends Mock
     implements NumberTriviarReqpository {}
+
+void main() {
+  GetConcreteNumberTriviar usecase;
+  MockNumberTriviarRespository mockNumberTriviarRespository;
+
+  setUp(() {});
+}
